ce31af0be61905c41c7c82d8b49275829bd76817
olaf <oalfolev@gmail.com>
2023-03-22 12:51:24 +0200

    WIP

--- index.js
+++ index.js
@@ -3,16 +3,7 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const yamlJs = require('yamljs');
 const swaggerDocument = yamlJs.load('./swagger.yaml');
-var mysql = require('mysql');
 
-
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "qwerty",
-    database: "logins"
-});
 
 
 
@@ -38,22 +29,12 @@ app.use((err, req, res, next) => {
 
 // End point for user registration
 app.post('/users', (req, res) => {
-    //
-    res.status(201).send('User registered');
+    let email = req.body.email;
+    let password = req.body.password;
 
-    let email = req.body.inputEmail
-    let password = req.body.inputPassword
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        var sql=`insert into logininfo('email','password') values (${email},${password})`;
-        con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("1 record inserted");
-        });
-    })
-})
+;
+});
 
 app.listen(port, () => {
-    console.log(`App running. Docs at http://localhost:${port}/docs`);
+    console.log(`App running. Docs at http://localhost:${port}`);
 })
